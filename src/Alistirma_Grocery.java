8a322a6bcff4c0b48cf107e22d572ad7716c4538
Emine Cicek <eminepurdy85@gmail.com>
2023-03-14 21:31:08 +0000

    Initial commit

---
+++ src/Alistirma_Grocery.java
@@ -0,0 +1,19 @@
+public class Alistirma_Grocery {
+    //
+    //Task://Write a programs of a grocery store that shows all the  7 day earning of the store on a daily basis.
+    //    //also write a method that shows the average amounth the store earns this week.
+    //    //also write a method that shows which days the store earns above the average.
+    //    //also write a method that shows which days the store earns below the average.
+    //
+    //    // step 1:Create an Array Containing the days (Days) Ahmet
+    //    //step 2:Create an ArraysList which we will add the daily earnings.emine
+    //    // step 3:use the whileloop and take the 7 day earnings from the user,one by one and add them to the daily earnings ArrayList Suzan
+    //    //step 4:get the average earning with the method called getAverageEarnings().Ahmet
+    //    //step 5:Create a metho called getOrtalamaninUstundeKazancGünleri(), Emine
+    //    //     Compare all days with average earnings with for loop
+    //    //      If higher than average earnings, return those days.
+    //    //step 6:Create a method called getUnderAverageEarningDays().Suzan
+    //    //     Compare all days with average earnings with for loop
+    //    //      If less than average earnings, return those days.
+
+}
